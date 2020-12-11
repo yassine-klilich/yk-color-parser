940c97921d9a65ce89f980f2b1b3798d4afdb9df
Yassine Klilich <klilichyassine8@gmail.com>
2020-12-11 21:16:39 +0100

    Add: HSV and HSVA compiler Fix: Add case insensitive for regex expressions Fix: Changed .startsWith() by a regex expression

--- color-compiler.js
+++ color-compiler.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function isString(value) {
    return (typeof(value) == "string" || (value instanceof String));
@@ -15,16 +15,16 @@ function colorCompiler(color) {
 
    let _color = color.trim();
 
-   if(_color.startsWith("rgb")) {
+   if(/^(rgb)/i.test(_color)) {
       return compileRgb(_color);
    }
-   if(_color.startsWith("hsv")) {
+   if(/^(hsv)/i.test(_color)) {
       return compileHsv(_color);
    }
-   if(_color.startsWith("hsl")) {
+   if(/^(hsl)/i.test(_color)) {
 
    }
-   if(_color.startsWith("#")) {
+   if(/^(#)/i.test(_color)) {
 
    }
 
@@ -35,8 +35,8 @@ function colorCompiler(color) {
 function compileRgb(rgbColor) {
    let red, green, blue;
 
-   if(/^(rgb)\s{0,}\(\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,}\)$/.test(rgbColor) 
-      || /^(rgba)\s{0,}\(\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]|(0\.([0-9]{1,})))\s{0,}\)$/.test(rgbColor)) {
+   if(/^(rgb)\s{0,}\(\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,}\)$/i.test(rgbColor) 
+      || /^(rgba)\s{0,}\(\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]{1,})\s{0,},\s{0,}([0-9]|(0\.([0-9]{1,})))\s{0,}\)$/i.test(rgbColor)) {
       let splitedValues = rgbColor.split(/([0-9]{1,})/);
       red = parseInt(splitedValues[1]);
       green = parseInt(splitedValues[3]);
@@ -52,7 +52,7 @@ function compileRgb(rgbColor) {
          throw new RangeError(`COMPILER_ERR:: '${rgbColor}' --> ${blue} is an invalid blue color, it must be an interger between 0 and 255`);
       }
 
-      if(rgbColor.startsWith("rgba")) {
+      if(/^(rgba)/i.test(rgbColor)) {
          let alpha = parseFloat(rgbColor.split(",")[3]);
    
          if(alpha > 1) {
@@ -66,20 +66,62 @@ function compileRgb(rgbColor) {
       }
    }
    else {
-      if(rgbColor.startsWith("rgba")) {
-         throw new Error(`COMPILER_ERR:: '${rgbColor}' is an invalid RGBA color value`);
+      if(/^(rgba)/i.test(rgbColor)) {
+         throw new SyntaxError(`COMPILER_ERR:: '${rgbColor}' is an invalid RGBA color value`);
       }
       else {
-         throw new Error(`COMPILER_ERR:: '${rgbColor}' is an invalid RGB color value`);
+         throw new SyntaxError(`COMPILER_ERR:: '${rgbColor}' is an invalid RGB color value`);
       }
    }
-
 }
 
-
-function compileHsv(color) {
+function compileHsv(hsvColor) {
+   let hue, saturate, value;
    
+   if(/^(hsv)\s{0,}\(\s{0,}([0-9]{1,})\s{0,}((deg)|(°)){0,1}\s{0,},\s{0,}([0-9]{1,})\s{0,}(%)\s{0,},\s{0,}([0-9]{1,})\s{0,}(%)\s{0,}\)$/i.test(hsvColor)
+      || /^(hsva)\s{0,}\(\s{0,}([0-9]{1,})\s{0,}((deg)|(°)){0,1}\s{0,},\s{0,}([0-9]{1,})\s{0,}(%)\s{0,},\s{0,}([0-9]{1,})\s{0,}(%)\s{0,},\s{0,}([0-9]|(0\.([0-9]{1,})))\s{0,}\)$/i.test(hsvColor)) {
+      let splitedValues = hsvColor.split(/([0-9]{1,})/);
+      hue = parseInt(splitedValues[1]);
+      saturate = parseInt(splitedValues[3]);
+      value = parseInt(splitedValues[5]);
+
+      if(hue > 360) {
+         throw new RangeError(`COMPILER_ERR:: '${hsvColor}' --> ${hue} is an invalid hue value, it must be an interger between 0 and 360`);
+      }
+      if(saturate > 100) {
+         throw new RangeError(`COMPILER_ERR:: '${hsvColor}' --> ${saturate} is an invalid saturate value, it must be an interger between 0 and 100`);
+      }
+      if(value > 100) {
+         throw new RangeError(`COMPILER_ERR:: '${hsvColor}' --> ${value} is an invalid value value, it must be an interger between 0 and 100`);
+      }
+
+      if(/^(hsva)/i.test(hsvColor)) {
+         let alpha = parseFloat(hsvColor.split(",")[3]);
+   
+         if(alpha > 1) {
+            throw new RangeError(`COMPILER_ERR:: '${hsvColor}' --> ${alpha} is an invalid alpha value, it must be an interger or a float number between 0 and 1`);
+         }
+   
+         return { hue, saturate, value, alpha };
+      }
+      else {
+         return { hue, saturate, value };
+      }
+   }
+   else {
+      if(/^(hsva)/i.test(hsvColor)) {
+         throw new SyntaxError(`COMPILER_ERR:: '${hsvColor}' is an invalid HSVA color value`);
+      }
+      else {
+         throw new SyntaxError(`COMPILER_ERR:: '${hsvColor}' is an invalid HSV color value`);
+      }
+   }
 }
 
-let color = colorCompiler('rgba(120, 240, 67, 1)');
+
+// let color = colorCompiler('rgba(120 240, 67, 1)');
+// let color = colorCompiler('hsva(120deg, 67%, 1%, 1)');
+// let color = colorCompiler('hsv(120°, 67%, 1%)');
+// let color = colorCompiler('hsva(360, 67%, 1%, 0)');
+let color = colorCompiler('hsva(120°, 67%, 1%, 0.031)');
 console.log(color);
